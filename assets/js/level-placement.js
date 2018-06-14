572e75f13afe2cef6da7437db1d87829fb94d1de
abdiangel <abdiangel.30@gmail.com>
2018-06-14 19:14:55 -0400

    fix(form): added fonts, imgs and styles that were needed

--- assets/js/level-placement.js
+++ assets/js/level-placement.js
@@ -1,99 +1,90 @@
 jQuery(document).ready(function ($) {
     var levelform = document.querySelector('#levelForm');
-    // var lvlsBar = document.querySelector('.progress-block');
-    // var nextLevel = document.querySelector('#next-questions');
-    // var prevLevel = document.querySelector('.prev-block');
-    // var startTest = document.querySelector('#startTest');
-    // var currentLvl = 1;
-    // var inputName = $('#testFullName');
-    // var inputEmail = $('#testEmail');
-    // var inputPhone = $('#testTelNo');
-    // var validEmail = false;
-    // var validFullName = false;
-    // var validPhone = false;
+    var lvlsBar = document.querySelector('.progress-block');
+    var nextLevel = document.querySelector('#next-questions');
+    var prevLevel = document.querySelector('.prev-block');
+    var startTest = document.querySelector('#startTest');
+    var currentLvl = 1;
+    var inputName = $('#testFullName');
+    var inputEmail = $('#testEmail');
+    var inputPhone = $('#testTelNo');
+    var validEmail = false;
+    var validFullName = false;
+    var validPhone = false;
 
+    var tabChanger = function (a) {
 
-    // var tabChanger = function (a) {
+        if (a == 'next') {
+            currentLvl++;
+        } else if (a == 'prev') {
+            currentLvl--;
+        }
 
-    //     if (a == 'next') {
-    //         currentLvl++;
-    //     } else if (a == 'prev') {
-    //         currentLvl--;
-    //     }
+        for (let i = 0; i < levelForm.children.length; i++) {
+            const item = levelForm.children.item(i);
+            const lvl = lvlsBar.children.item(i);
+            const lvlIndex = $(lvl).index();
 
-    //     for (let i = 0; i < levelForm.children.length; i++) {
-    //         const item = levelForm.children.item(i);
-    //         const lvl = lvlsBar.children.item(i);
-    //         const lvlIndex = $(lvl).index();
+            if (parseInt(item.dataset['step']) === currentLvl) {
+                $(item).show();
+                if ($('.lvl--active')) {
+                    $('.lvl--active').removeClass('lvl--active');
+                }
+                $(lvl).addClass('lvl--active');
+                $(prevLevel).show();
 
-    //         if (parseInt(item.dataset['step']) === currentLvl) {
-    //             $(item).show();
-    //             if ($('.lvl--active')) {
-    //                 $('.lvl--active').removeClass('lvl--active');
-    //             }
-    //             $(lvl).addClass('lvl--active');
-    //             $(prevLevel).show();
+            } else if (parseInt(item.dataset['step']) === currentLvl && lvlIndex) {
+                $(item).show();
+                if ($(lvl).hasClass('lvl--active')) {
+                    $(lvl).removeClass('lvl--active');
+                }
+                $(lvl).addClass('lvl--active');
+                $(prevLevel).show();
 
-    //         } else if (parseInt(item.dataset['step']) === currentLvl && lvlIndex) {
-    //             $(item).show();
-    //             if ($(lvl).hasClass('lvl--active')) {
-    //                 $(lvl).removeClass('lvl--active');
-    //             }
-    //             $(lvl).addClass('lvl--active');
-    //             $(prevLevel).show();
+            } else {
+                $(item).hide();
+            }
+        }
+        console.log(currentLvl);
+    }
 
-    //         } else {
-    //             $(item).hide();
-    //         }
-    //     }
-    //     console.log(currentLvl);
-    // }
+    nextLevel.addEventListener('click', function () {
+        tabChanger('next');
+    });
 
-    // nextLevel.addEventListener('click', function () {
-    //     tabChanger('next');
-    // });
+    prevLevel.addEventListener('click', function () {
+        tabChanger('prev');
+    });
 
-    // prevLevel.addEventListener('click', function () {
-    //     tabChanger('prev');
-    // });
+    $(inputName).on('input', function () {
+        var input = $(this);
+        var is_name = input.val();
+        if (is_name) {
+            validFullName = true;
+        } else {
+            validFullanem = false;
+        }
+    });
 
-    // // startTest.addEventListener('click', function () {
-    // //     $('.landing-container').hide();
-    // //     $('.wizard-container').show();
-    // // });
+    $(inputEmail).on('input', function () {
+        var input = $(this);
+        var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var is_email = re.test(input.val());
+        if (is_email) {
+            validEmail = true;
+        } else {
+            validEmail = false;
+        }
+    });
 
-    // $(inputName).on('input', function () {
-    //     var input = $(this);
-    //     var is_name = input.val();
-    //     if (is_name) {
-    //         validFullName = true;
-    //     } else {
-    //         validFullanem = false;
-    //     }
-    // });
-
-    // $(inputEmail).on('input', function () {
-    //     var input = $(this);
-    //     var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    //     var is_email = re.test(input.val());
-    //     if (is_email) {
-    //         validEmail = true;
-    //     } else {
-    //         validEmail = false;
-    //     }
-    // });
-
-    // $(inputPhone).on('input', function () {
-    //     var input = $(this);
-    //     // var re =
-    //     //     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-    //     // var is_tel = re.test(input.val());
-    //     if ($(input).val()) {
-    //         validPhone = true;
-    //     } else {
-    //         validPhone = false;
-    //     }
-    // });
+    $(inputPhone).on('input', function () {
+        var input = $(this);
+        if ($(input).val()) {
+            validPhone = true;
+        } else {
+            validPhone = false;
+        }
+    });
 
     function splitIntoSubArray(arr, count) {
         var newArray = [];
@@ -103,46 +94,32 @@ jQuery(document).ready(function ($) {
         return newArray;
     }
 
-    // $("#startTest").click(function (event) {
+    $("#startTest").click(function (event) {
 
-    //     if (validFullName === true && validEmail === true && validPhone === true) {
+        if (validFullName === true && validEmail === true && validPhone === true) {
 
-    //         var nameVal = $(inputName).val();
-    //         var emailVal = $(inputEmail).val();
-    //         var phoneVal = $(inputPhone).val();
+            var nameVal = $(inputName).val();
+            var emailVal = $(inputEmail).val();
+            var phoneVal = $(inputPhone).val();
 
-    //         $('.landing-container').hide();
-    //         $('.wizard-container').show();
-
-    //         console.log(nameVal);
-    //         console.log(emailVal);
-    //         console.log(phoneVal);
+            $('.landing-container').hide();
+            $('.wizard-container').show();
 
             $.getJSON( "http://localhost/Speaktusucceed/wp-json/levelplacement/v1/tests/category/22", function( data ) {
                 var questions = [];
                 $.each( data, function( key, val ) {
                     questions.push(val);
-                    // // console.log(val.title); TITLE OF QUESTION
-                    // $.each(val.meta, function( keyTwo, valTwo) {
-
-                    //     $.each(valTwo, function( keyThree, valThree) {
-                    //        // console.log(valThree.text); THIS IS THE ANSWER
-                    //     });
-                    // });
                 });
                 var fases = splitIntoSubArray(questions,10);
-                console.log(fases);
                 fases.map(function(item) {
-                    console.log(item);
-                    $(levelform).append(`<div class="debug-border"></div>`);
+                    // $(levelform).append(`<div class="debug-border"></div>`);
                     item.map(function(item2) {
-                        console.log(item2.title);
-                        console.log(item2);
-                        $('.debug-burder').append(`<div class="debug-border">${item.title}</div>`);
+                        console.log(item2.meta.answers[0]);
+                        // $('.debug-border').append(`<div class="debug-border">${item2.title}</div>`);
                     });
                 });
             });
-    //     }
-    // });
+        }
+    });
 
 });
